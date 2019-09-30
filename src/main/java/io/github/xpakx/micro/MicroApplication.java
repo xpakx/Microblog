b7435022eed54630efec69e6ddc62aa693e44955
xpakx <55586470+xpakx@users.noreply.github.com>
2019-09-30 12:57:27 +0200

    feat: add thymeleaf dialect to show new lines in posts

--- src/main/java/io/github/xpakx/micro/MicroApplication.java
+++ src/main/java/io/github/xpakx/micro/MicroApplication.java
@@ -2,12 +2,20 @@ package io.github.xpakx.micro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import io.github.xpakx.micro.dialect.CustomDialect;
 
 @SpringBootApplication
 public class MicroApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MicroApplication.class, args);
-	}
+	 public static void main(String[] args) 
+  {
+		  SpringApplication.run(MicroApplication.class, args);
+	 }
 
+  @Bean
+  public CustomDialect customDialect()
+  {
+    return new CustomDialect();
+  }
 }
---
+++ src/main/java/io/github/xpakx/micro/dialect/PostMessageAttributeProcessor.java
@@ -0,0 +1,38 @@
+package io.github.xpakx.micro.dialect;
+
+import org.thymeleaf.IEngineConfiguration;
+import org.thymeleaf.context.ITemplateContext;
+import org.thymeleaf.engine.AttributeName;
+import org.thymeleaf.model.IProcessableElementTag;
+import org.thymeleaf.processor.element.AbstractAttributeTagProcessor;
+import org.thymeleaf.processor.element.IElementTagStructureHandler;
+import org.thymeleaf.standard.expression.IStandardExpression;
+import org.thymeleaf.standard.expression.IStandardExpressionParser;
+import org.thymeleaf.standard.expression.StandardExpressions;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.unbescape.html.HtmlEscape;
+
+public class PostMessageAttributeProcessor extends AbstractAttributeTagProcessor 
+{
+  private static final String ATTR_NAME = "process";
+  private static final int PRECEDENCE = 10000;
+  
+  public PostMessageAttributeProcessor(final String dialectPrefix) 
+  {
+    super(TemplateMode.HTML, dialectPrefix, null, false, ATTR_NAME, true, PRECEDENCE,        true);   
+  }
+
+  protected void doProcess(final ITemplateContext context, final IProcessableElementTag tag, final AttributeName attributeName, final String attributeValue, final IElementTagStructureHandler structureHandler) 
+  {
+    final IEngineConfiguration configuration = context.getConfiguration();
+    final IStandardExpressionParser parser = StandardExpressions.getExpressionParser(configuration);
+    final IStandardExpression expression = parser.parseExpression(context, attributeValue);
+    final String message = (String) expression.execute(context);
+    final String messageWithoutHTML = HtmlEscape.escapeHtml5(message);
+    final String finalMessage = messageWithoutHTML
+      .replace("\n", "<br />");
+    structureHandler.setBody(finalMessage, false);
+  }
+
+
+}
