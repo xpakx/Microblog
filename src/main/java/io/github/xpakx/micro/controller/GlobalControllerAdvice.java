89f8a9466d14e873a81241cf1d95cc040359ef78
xpakx <55586470+xpakx@users.noreply.github.com>
2019-10-04 16:48:59 +0200

    feat: add global model attribute

--- src/main/java/io/github/xpakx/micro/controller/GlobalControllerAdvice.java
+++ src/main/java/io/github/xpakx/micro/controller/GlobalControllerAdvice.java
@@ -1,14 +1,29 @@
 package io.github.xpakx.micro.controller;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.ui.Model;
-import org.springframework.web.servlet.ModelAndView;
+import java.security.Principal;
+import io.github.xpakx.micro.entity.User;
+import io.github.xpakx.micro.service.NotificationService;
+import io.github.xpakx.micro.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Optional;
 
 @ControllerAdvice 
 public class GlobalControllerAdvice 
 {
+  private NotificationService notificationService;
+  private UserService userService;
+  
+  @Autowired
+  public GlobalControllerAdvice(UserService userService, NotificationService notificationService)
+  {
+    this.userService = userService;
+    this.notificationService = notificationService;
+  }
 
   @ExceptionHandler(Exception.class)
   public String defaultErrorHandler(HttpServletRequest request, Exception e, Model model)
@@ -17,5 +32,21 @@ public class GlobalControllerAdvice
     model.addAttribute("url", request.getRequestURL());
     return "error";
   }
-
+  
+  @ModelAttribute("notifcount")
+  public Integer getNotificationCountForCurrentUser(Principal principal) 
+  {
+    Optional<User> user = userService.findByUsername(principal.getName());
+    
+    if(user.isPresent())
+    {
+      return notificationService.getNewNotificationsCount(user.get().getId());
+    }
+    else
+    {
+      return 0;
+    }
+    
+    
+  }
 }
